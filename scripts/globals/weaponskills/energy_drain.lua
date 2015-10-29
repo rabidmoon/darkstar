f5a8fe40ae73483e4cb615705d94abc5aa9ce820
Tummie <tummie_garuda@hotmail.com>
2015-10-29 13:46:06 -0700

    Energy Steal/Drain
    Added formula for energy steal and energy drain

--- scripts/globals/weaponskills/energy_drain.lua
+++ scripts/globals/weaponskills/energy_drain.lua
@@ -3,6 +3,16 @@
 ------------------------
 
 function onUseWeaponSkill(player, target, wsID)
-   -- TODO: Should steal MP based on TP and not wake the mob involved.
-	return 1, 0, false, 65;
+   local params = {};
+	params.ftp100 = 1.25; params.ftp200 = 2; params.ftp300 = 2.5;
+	params.str_wsc = 0.0; params.dex_wsc = 0.0; params.vit_wsc = 0.0; params.agi_wsc = 0.0; params.int_wsc = 0.0; params.mnd_wsc = 1.0; params.chr_wsc = 0.0;
+	params.ele = ELE_DARK;
+	params.skill = SKILL_DAG;
+	params.includemab = true;
+
+
+	local damage, criticalHit, tpHits, extraHits = doMagicWeaponskill(player, target, params);
+	damage = damage * WEAPON_SKILL_POWER
+	player:addMP(damage);
+	return tpHits, extraHits, criticalHit, damage;
 end
