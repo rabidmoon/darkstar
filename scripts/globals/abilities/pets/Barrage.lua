a29c7700dbd9a9d7a5b0f0377b8172dcbe30b86a
Tummie <tummie_garuda@hotmail.com>
2015-11-25 08:43:40 -0800

    Sharpshot Frame Barrage Reduction
    Reduced number of barrage hits capable on Barrage

--- scripts/globals/abilities/pets/Barrage.lua
+++ scripts/globals/abilities/pets/Barrage.lua
@@ -19,9 +19,9 @@ local barrage = 0;
 local player = pet:getMaster();
 local wind = player:getEffectsCount(EFFECT_WIND_MANEUVER);
 if (wind == 1) then
-barrage = 4;
+barrage = 3;
 elseif (wind == 2) then
-barrage = 6;
+barrage = 5;
 elseif (wind == 3) then
 barrage = 8;
 else
@@ -32,14 +32,17 @@ end
 
 
     local numhits = barrage;
+	local basemod = 1;
     local accmod = 1;
+	local attmod = 1;
 	local str_wsc = 0;
 	local dex_wsc = 0.5;
 	local agi_wsc = 0;
 	local vit_wsc = 0;
+	local wsc_mnd = 0;
 
 	
-	local info = AutoRangedMove(pet,target,skill,numhits,accmod,str_wsc,dex_wsc,agi_wsc,vit_wsc,TP_NO_EFFECT);
+	local info = AutoRangedMove(pet,target,skill,basemod,numhits,attmod,accmod,str_wsc,dex_wsc,agi_wsc,vit_wsc,wsc_mnd,TP_NO_EFFECT);
  
     local dmg = MobFinalAdjustments(info.dmg,pet,skill,target,MOBSKILL_RANGED,MOBPARAM_PIERCE,info.hitslanded);
 	
