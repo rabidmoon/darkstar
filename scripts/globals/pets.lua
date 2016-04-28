299b7b9ac8ce30420c33e8df5a25a8f7ea4f1a9f
Tummie <tummie_garuda@hotmail.com>
2016-04-27 19:41:22 -0700

    Added pettype and Disband command
    Trusts can be desummoned with @disband command.  Only disbands the most
    recent trust summoned

--- scripts/globals/pets.lua
+++ scripts/globals/pets.lua
@@ -15,6 +15,7 @@ PETTYPE_CHARMED_MOB       = 3;
 PETTYPE_AUTOMATON         = 4;
 PETTYPE_ADVENTURING_FELLOW= 5;
 PETTYPE_CHOCOBO           = 6;
+PETTYPE_TRUST             = 7;
 
 
 -----------------------------------
