ff3c2e21e910cd40041d18ce1ec76a14adf68d8d
Tummie <tummie_garuda@hotmail.com>
2016-05-03 13:08:39 -0700

    Fixed Trust not Spawning

--- scripts/globals/spells/curilla.lua
+++ scripts/globals/spells/curilla.lua
@@ -11,8 +11,8 @@ require("scripts/globals/summon");
 -----------------------------------------
 
 function onMagicCastingCheck(caster,target,spell)
-    local caster:getZoneID();
-	If (zone == 185) or (zone == 186) or (zone == 187) then
+    local zone = caster:getZoneID();
+	if (zone == 185) or (zone == 186) or (zone == 187) then
 	caster:PrintToPlayer("You cannot summon a trust in this area",0xD);
 	end
 	
