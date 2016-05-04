cec083d70afdfc0b0f451c55e3aa55854542dac9
Tummie <tummie_garuda@hotmail.com>
2016-05-04 13:04:38 -0700

    Ayame trust obtain
    Ayame will now become a trust for other nations.  Was missing rank
    requirement

--- scripts/zones/Metalworks/npcs/Ayame.lua
+++ scripts/zones/Metalworks/npcs/Ayame.lua
@@ -39,8 +39,10 @@ function onTrigger(player,npc)
 	local WildcatBastok = player:getVar("WildcatBastok");
 	local pNation = player:getNation();
 	local currentMission = player:getCurrentMission(pNation);
+	local srank = getNationRank(SANDORIA);
+	local wrank = getNationRank(WINDURST);
 	
-		if (player:getNation() == 0) and (player:hasKeyItem(RED_INSTITUTE_CARD)) and (player:hasSpell(900) == false) and (currentMission == JOURNEY_TO_WINDURST) then  -- Sandy Nation and mission 2-3
+	if (player:getNation() == 0) and (player:hasKeyItem(RED_INSTITUTE_CARD)) and (player:hasSpell(900) == false) and (currentMission == JOURNEY_TO_WINDURST) then  -- Sandy Nation and mission 2-3
 	player:PrintToPlayer("Your Red Institute Card flashes brilliantly!", 0x1C);
     player:PrintToPlayer("Ayame : Ah a Red Institute Card.  My blade is ready to assist you", 0xD);
     player:addSpell(900);	
@@ -49,9 +51,17 @@ function onTrigger(player,npc)
     player:PrintToPlayer("Ayame : Ah a Red Institute Card.  My blade is ready to assist you", 0xD);
     player:addSpell(900);
     elseif (player:getNation() == 2) and (player:hasKeyItem(GREEN_INSTITUTE_CARD)) and (player:hasSpell(900) == false) and (currentMission == THE_THREE_KINGDOMS) then -- Windy Nation can obtain without pre-requisites
-	player:PrintToPlayer("Your Blue Institute Card flashes brilliantly!", 0x1C);
+	player:PrintToPlayer("Your Green Institute Card flashes brilliantly!", 0x1C);
     player:PrintToPlayer("Ayame : Ah a Green Institute Card.  My blade is ready to assist you", 0xD);
     player:addSpell(900);	
+    elseif (player:getNation() == 2) and (player:hasKeyItem(GREEN_INSTITUTE_CARD)) and (player:hasSpell(900) == false) and (wrank >= 3) then -- Windy Nation can obtain when rank is higher than 2
+	player:PrintToPlayer("Your Green Institute Card flashes brilliantly!", 0x1C);
+    player:PrintToPlayer("Ayame : Ah a Green Institute Card.  My blade is ready to assist you", 0xD);
+    player:addSpell(900);	
+	elseif (player:getNation() == 0) and (player:hasKeyItem(RED_INSTITUTE_CARD)) and (player:hasSpell(900) == false) and (srank >= 3) then  -- Sandy Nation can obtain when rank is higher than 2
+	player:PrintToPlayer("Your Red Institute Card flashes brilliantly!", 0x1C);
+    player:PrintToPlayer("Ayame : Ah a Red Institute Card.  My blade is ready to assist you", 0xD);
+    player:addSpell(900);
 	end
 	
 	
