7bdc597d10c2f7668d8e31415e3eaab25dc592ce
Tummie <tummie_garuda@hotmail.com>
2016-08-18 12:30:37 -0700

    August 5th TextID's
    Updated text ID"s to the new retail verison

--- scripts/zones/Tahrongi_Canyon/TextIDs.lua
+++ scripts/zones/Tahrongi_Canyon/TextIDs.lua
@@ -7,32 +7,35 @@ ITEM_CANNOT_BE_OBTAINED = 6560; -- You cannot obtain the item <item> come back a
        KEYITEM_OBTAINED = 6568; -- Obtained key item: <keyitem>.
 
  FISHING_MESSAGE_OFFSET = 7226; -- You can't fish here.
-  ALREADY_OBTAINED_TELE = 7321; -- You already possess the gate crystal for this telepoint.
+  ALREADY_OBTAINED_TELE = 7326; -- You already possess the gate crystal for this telepoint.
         NOTHING_HAPPENS = 300; -- Nothing happens.
 
 -- Mining
-MINING_IS_POSSIBLE_HERE = 7423; -- Mining is possible here if you have
+MINING_IS_POSSIBLE_HERE = 7428; -- Mining is possible here if you have
 
 -- ??? that spawns NM Yara Ma Yha Who
-     SPROUT_LOOKS_WITHERED = 7547; -- There is something sprouting from the ground here. It looks a little withered.
-REPULSIVE_CREATURE_EMERGES = 7548; -- A repulsive creature emerges from the ground!
-SPROUT_DOES_NOT_NEED_WATER = 7549; -- The sprout does not need any more water now.
-     SPROUT_LOOKING_BETTER = 7551; -- The sprout is looking better.
-     
+     SPROUT_LOOKS_WITHERED = 7546; -- There is something sprouting from the ground here. It looks a little withered.
+REPULSIVE_CREATURE_EMERGES = 7547; -- A repulsive creature emerges from the ground!
+SPROUT_DOES_NOT_NEED_WATER = 7548; -- The sprout does not need any more water now.
+     SPROUT_LOOKING_BETTER = 7550; -- The sprout is looking better.
+
 -- Tahrongi Cactus
-            BUD_BREAKS_OFF = 7400; -- The bud breaks off. You obtain
-    POISONOUS_LOOKING_BUDS = 7401; -- The flowers have poisonous-looking buds.
-        CANT_TAKE_ANY_MORE = 7402; -- You can't take any more.
+            BUD_BREAKS_OFF = 7405; -- The bud breaks off. You obtain
+    POISONOUS_LOOKING_BUDS = 7406; -- The flowers have poisonous-looking buds.
+        CANT_TAKE_ANY_MORE = 7407; -- You can't take any more.
+
+-- Luck Rune
+NOTHING_OUT_OF_THE_ORDINARY = 6579;  -- There is nothing out of the ordinary here.
 
 -- Other Texts
-TELEPOINT_HAS_BEEN_SHATTERED = 7504; -- The telepoint has been shattered into a thousand pieces...
-       TELEPOINT_DISAPPEARED = 7322; -- The telepoint has disappeared...
+TELEPOINT_HAS_BEEN_SHATTERED = 7503; -- The telepoint has been shattered into a thousand pieces...
+       TELEPOINT_DISAPPEARED = 7327; -- The telepoint has disappeared...
 
 -- Signs
-SIGN_1 = 7396; -- North: Meriphataud Mountains Northeast: Crag of Mea South: East Sarutabaruta
-SIGN_3 = 7397; -- North: Meriphataud Mountains East: Crag of Mea South: East Sarutabaruta
-SIGN_5 = 7398; -- North: Meriphataud Mountains East: Buburimu Peninsula South: East Sarutabaruta
-SIGN_7 = 7399; -- East: Buburimu Peninsula West: East Sarutabaruta
+SIGN_1 = 7401; -- North: Meriphataud Mountains Northeast: Crag of Mea South: East Sarutabaruta
+SIGN_3 = 7402; -- North: Meriphataud Mountains East: Crag of Mea South: East Sarutabaruta
+SIGN_5 = 7403; -- North: Meriphataud Mountains East: Buburimu Peninsula South: East Sarutabaruta
+SIGN_7 = 7404; -- East: Buburimu Peninsula West: East Sarutabaruta
 
 -- conquest Base
 CONQUEST_BASE = 0;
@@ -40,4 +43,3 @@ CONQUEST_BASE = 0;
 -- chocobo digging
 DIG_THROW_AWAY = 7239; -- You dig up ?Possible Special Code: 01??Possible Special Code: 01??Possible Special Code: 01? ?Possible Special Code: 01??Possible Special Code: 05?$?BAD CHAR: 8280??BAD CHAR: 80??BAD CHAR: 80?, but your inventory is full.
 FIND_NOTHING = 7241; -- You dig and you dig, but find nothing.
-
