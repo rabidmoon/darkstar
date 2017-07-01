110e36f3dd19024b32297567c8beea62f69e520c
Tummie <tummie_garuda@hotmail.com>
2017-06-30 17:54:16 -0700

    Text ID Update
    Text IDs updated from April Patch.  Needs new sync for latest June Patch

--- scripts/zones/Ghelsba_Outpost/TextIDs.lua
+++ scripts/zones/Ghelsba_Outpost/TextIDs.lua
@@ -1,18 +1,18 @@
 -- Variable TextID   Description text
 
 -- General Texts
-ITEM_CANNOT_BE_OBTAINED = 6907; -- You cannot obtain the item <item>. Come back after sorting your inventory.
-          ITEM_OBTAINED = 6912; -- Obtained: <item>.
-           GIL_OBTAINED = 6913; -- Obtained <number> gil.
-       KEYITEM_OBTAINED = 6915; -- Obtained key item: <keyitem>.
- FISHING_MESSAGE_OFFSET = 7573; -- You can't fish here.
+ITEM_CANNOT_BE_OBTAINED = 6908; -- You cannot obtain the item <item>. Come back after sorting your inventory.
+          ITEM_OBTAINED = 6914; -- Obtained: <item>.
+           GIL_OBTAINED = 6915; -- Obtained <number> gil.
+       KEYITEM_OBTAINED = 6917; -- Obtained key item: <keyitem>.
+ FISHING_MESSAGE_OFFSET = 7575; -- You can't fish here.
 
 -- Logging
-LOGGING_IS_POSSIBLE_HERE = 7734; -- Logging is possible here if you have
+LOGGING_IS_POSSIBLE_HERE = 7736; -- Logging is possible here if you have
 
 -- Quest Dialogs
 YOU_CANNOT_ENTER_THE_BATTLEFIELD = 162; -- You cannot enter the battlefield at present. Please wait a little longer.
-    YOU_CAN_NOW_BECOME_A_DRAGOON = 7774; -- You can now become a dragoon!
+    YOU_CAN_NOW_BECOME_A_DRAGOON = 7776; -- You can now become a dragoon!
 
 -- conquest Base
 CONQUEST_BASE = 0;
