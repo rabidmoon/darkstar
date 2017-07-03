6a6f26c077cdb8345e86aa456314e174eac335f5
Tummie <tummie_garuda@hotmail.com>
2017-07-03 14:25:12 -0700

    Key Item expansion
    Key items above ID 2560 would not work.

--- src/common/mmo.h
+++ src/common/mmo.h
@@ -144,8 +144,8 @@ struct skills_t
 
 struct keyitems_t 
 {
-	 uint8 keysList[320];	// таблица ключевых предметов
-	 uint8 seenList[320];	// таблица ключевых предметов, отмеченных как "увиденные" 
+	 uint8 keysList[512];	// таблица ключевых предметов
+	 uint8 seenList[512];	// таблица ключевых предметов, отмеченных как "увиденные" 
 };
 
 struct position_t 
