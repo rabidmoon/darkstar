b89abad98fd19d3527d6d206ba5e6ee3c81cdb0b
Tummie <tummie_garuda@hotmail.com>
2017-07-14 15:30:47 -0700

    Trust Spawn in DynaXarc
    Fixed an issue where Trusts could not be spawned in Dynamis Xarc

--- scripts/globals/spells/curilla.lua
+++ scripts/globals/spells/curilla.lua
@@ -13,12 +13,12 @@ require("scripts/globals/summon");
 function onMagicCastingCheck(caster,target,spell)
  local zone = caster:getZoneID();
 	local dynamis = caster:getVar("TrustDynamis");
-	if (zone == 185) or (zone == 186) or (zone == 187) and (dynamis ~= 1) then
+	if (((zone == 185) or (zone == 186) or (zone == 187) or (zone == 188) or (zone == 134) or (zone == 135)) and (dynamis ~= 1)) then
 	caster:PrintToPlayer("You cannot summon a trust in this area",0xD);
 	else if(not caster:canUsePet()) then
 		return MSGBASIC_CANT_BE_USED_IN_AREA;
 	else if (not caster:isUniqueAlly(79)) then
-       	caster:PrintToPlayer("Kupipi is already summoned.",0xD);
+       	caster:PrintToPlayer("Curilla is already summoned.",0xD);
     else
 	return 0;
 	end
