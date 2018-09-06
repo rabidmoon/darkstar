34c132ec53942020c0fa02d0aa9abf912571262d
Tummie <tummie_garuda@hotmail.com>
2018-09-06 10:05:15 -0700

    Text ID Update
    Thanks SE

--- scripts/zones/Al_Zahbi/TextIDs.lua
+++ scripts/zones/Al_Zahbi/TextIDs.lua
@@ -4,42 +4,27 @@
   ITEM_CANNOT_BE_OBTAINED = 6381; -- You cannot obtain the item <item> come back again after sorting your inventory
             ITEM_OBTAINED = 6387; -- Obtained: <item>
              GIL_OBTAINED = 6388; -- Obtained <number> gil
-         KEYITEM_OBTAINED = 6389; -- Obtained key item: <keyitem>
-   FISHING_MESSAGE_OFFSET = 7047; -- You can't fish here
-        MOG_LOCKER_OFFSET = 7381; -- Your Mog Locker lease is valid until <timestamp>, kupo.
-            HOMEPOINT_SET = 7527; -- Home point set!
-     IMAGE_SUPPORT_ACTIVE = 7550; -- You have to wait a bit longer before asking for synthesis image support again.
-            IMAGE_SUPPORT = 7552; -- Your ?Multiple Choice (Parameter 1)?[fishing/woodworking/smithing/goldsmithing/clothcraft/leatherworking/bonecraft/alchemy/cooking] skills went up ...
-                 SANCTION = 7971; -- You have received the Empire's Sanction.
+         KEYITEM_OBTAINED = 6390; -- Obtained key item: <keyitem>
+   FISHING_MESSAGE_OFFSET = 7048; -- You can't fish here
+        MOG_LOCKER_OFFSET = 7382; -- Your Mog Locker lease is valid until <timestamp>, kupo.
+            HOMEPOINT_SET = 7528; -- Home point set!
+     IMAGE_SUPPORT_ACTIVE = 7551; -- You have to wait a bit longer before asking for synthesis image support again.
+            IMAGE_SUPPORT = 7553; -- Your ?Multiple Choice (Parameter 1)?[fishing/woodworking/smithing/goldsmithing/clothcraft/leatherworking/bonecraft/alchemy/cooking] skills went up ...
+                 SANCTION = 7972; -- You have received the Empire's Sanction.
 
  --Other Texts
-     ITEM_DELIVERY_DIALOG = 7833; -- No need to wrap your goods. Just hand them over and they're as good as delivered! (I've got to be nice as long as the manager's got his eye on me...)
+     ITEM_DELIVERY_DIALOG = 7834; -- No need to wrap your goods. Just hand them over and they're as good as delivered! (I've got to be nice as long as the manager's got his eye on me...)
 
 -- Shop Texts
-       ALLARD_SHOP_DIALOG = 7617; -- Hey, how ya doin'? Welcome to the armor shop of the Ulthalam Parade's leading star--Allard, in the flesh!
-      CHAYAYA_SHOP_DIALOG = 7629; -- Chayaya's Projectiles! Get your darts and more at Chayaya's Projectiles! Just don't touch the stuff in the high drawers, okay
-KAHAHHOBICHAI_SHOP_DIALOG = 7611; -- Step rrright up to Kahah Hobichai's Blades! We've got everything your battle-thirrrsty heart desires!
-        ZAFIF_SHOP_DIALOG = 7623; -- Welcome... I'm Zafif, and this is my magic shop... I hope you can find something of use here.
+       ALLARD_SHOP_DIALOG = 7618; -- Hey, how ya doin'? Welcome to the armor shop of the Ulthalam Parade's leading star--Allard, in the flesh!
+      CHAYAYA_SHOP_DIALOG = 7630; -- Chayaya's Projectiles! Get your darts and more at Chayaya's Projectiles! Just don't touch the stuff in the high drawers, okay
+KAHAHHOBICHAI_SHOP_DIALOG = 7612; -- Step rrright up to Kahah Hobichai's Blades! We've got everything your battle-thirrrsty heart desires!
+        ZAFIF_SHOP_DIALOG = 7624; -- Welcome... I'm Zafif, and this is my magic shop... I hope you can find something of use here.
 
- DEHBI_MOSHAL_SHOP_DIALOG = 7837; -- Welcome to the Carpenters' Guild!
-        NDEGO_SHOP_DIALOG = 7839; -- The Blacksmiths' Guild thanks you for your business!
-      BORNAHN_SHOP_DIALOG = 7841; -- Welcome! We have all your goldsmithing needs right here!
- TATEN_BILTEN_SHOP_DIALOG = 7843; -- Weave something beautiful with the materials you buy here, okay?
+ DEHBI_MOSHAL_SHOP_DIALOG = 7838; -- Welcome to the Carpenters' Guild!
+        NDEGO_SHOP_DIALOG = 7840; -- The Blacksmiths' Guild thanks you for your business!
+      BORNAHN_SHOP_DIALOG = 7842; -- Welcome! We have all your goldsmithing needs right here!
+ TATEN_BILTEN_SHOP_DIALOG = 7844; -- Weave something beautiful with the materials you buy here, okay?
 
 -- NPC Texts
-            CHOCOBO_HAPPY = 7846; -- The chocobo appears to be extremely happy.
-
--- Besieged Texts
-             MAMOOL_MARCH = 8081;
-              TROLL_MARCH = 8082;          
-             UNDEAD_MARCH = 8083;
-             MAMOOL_START = 8084;
-			  TROLL_START = 8085; 
-             UNDEAD_START = 8086;  -- Al Zahbi is in Besieged!
-           BESIEGED_START = 8087;  -- Al Zahbi is in Besieged!
-		      MAMOOL_LOSE = 8090;
-		       TROLL_LOSE = 8091;
-		      UNDEAD_LOSE = 8092;  -- Undead has retreated		  
-			   MAMOOL_WIN = 8095;
-			    TROLL_WIN = 8096;
-               UNDEAD_WIN = 8097;  -- The Hall of Binding has been breached, The undead swarp has taken the astral candy
+            CHOCOBO_HAPPY = 7847; -- The chocobo appears to be extremely happy.
--- scripts/zones/Apollyon/TextIDs.lua
+++ scripts/zones/Apollyon/TextIDs.lua
@@ -4,7 +4,7 @@
 ITEM_CANNOT_BE_OBTAINED = 6381; -- You cannot obtain the item <item> come back again after sorting your inventory
           ITEM_OBTAINED = 6387; -- Obtained: <item>
            GIL_OBTAINED = 6388; -- Obtained <number> gil
-       KEYITEM_OBTAINED = 6389; -- Obtained key item: <keyitem>
+       KEYITEM_OBTAINED = 6390; -- Obtained key item: <keyitem>
 
    CONDITION_FOR_LIMBUS = 7054;    -- You have clearance to enter Limbus, but cannot enter while you or a party member is engaged in battle.
              CHIP_TRADE = 7022;              --
@@ -12,5 +12,5 @@ ITEM_CANNOT_BE_OBTAINED = 6381; -- You cannot obtain the item <item> come back a
 -- Cannot find correct TextID for CHIP_TRADE
 
 -- conquest Base
-          CONQUEST_BASE = 7364; -- Tallying conquest results...
+          CONQUEST_BASE = 7365; -- Tallying conquest results...
 
